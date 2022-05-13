b2dfd07a8cf0a034790ba649594faa938bc48303
AntonYefanov <yefanov.anton@yandex.ru>
2022-05-13 14:44:01 +0300

    task folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+
+export enum TaskState {
+  undone = "undone",
+  done = "done",
+  failed = "failed"
+};
+
+export interface Task {
+  title: string;
+  description: string;
+  date: Date;
+  state: TaskState;
+};
+
+export interface ChangedState {
+  index: number;
+  state: TaskState;
+}
+
+export interface ChangedTask {
+  index: number;
+  task: Task;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +29,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'to-do-list';
+  tasks: Task[] = [{title: "Придумать что делать", description: "Добавить новых заданий", date: new Date(), state: TaskState.undone} ];
+  taskTitle: string = "";
+  taskDescription: string = "";
+  toggle:boolean = false;
+
+  constructor() {
+    let temp = localStorage.getItem("tasksArray");
+    if (temp != null){
+      this.tasks = JSON.parse(temp);
+    }    
+  }
+
+  showAddForm(){    
+    this.toggle = !this.toggle;
+  }
+
+  addTask(taskTitle: string, taskDescription: string) {
+    if (taskTitle !== "") {
+      let newTask: Task = {title: taskTitle, description: taskDescription, date: new Date(), state: TaskState.undone};
+      this.tasks.push(newTask);    
+
+      localStorage.setItem("tasksArray", JSON.stringify(this.tasks));
+
+      this.taskTitle = "";
+      this.taskDescription = "";
+      this.toggle = false;
+    } else {
+      this.taskDescription = "Введите название";
+    }    
+  }
+
+  onDelete(del:any) {
+    this.tasks.splice(del, 1);
+    localStorage.setItem("tasksArray", JSON.stringify(this.tasks));
+  }
+
+  onStateChanged(chs: ChangedState) {
+    this.tasks[chs.index].state = chs.state;
+    localStorage.setItem("tasksArray", JSON.stringify(this.tasks));
+  }
+
+  onTaskChanged(cht: ChangedTask) {
+    this.tasks[cht.index] = cht.task;
+    localStorage.setItem("tasksArray", JSON.stringify(this.tasks));
+  }
 }
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TaskState, Task, ChangedState, ChangedTask } from '../app.component';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent implements OnInit {
+
+  @Input() task: Task = {title: "", description: "", date: new Date(), state: TaskState.undone};
+  @Input() index: number = 0;  
+  @Output() onDelete = new EventEmitter<number>();
+  @Output() onStateChanged = new EventEmitter<ChangedState>();
+  @Output() onTaskChanged = new EventEmitter<ChangedTask>();
+  showCahngeForm: boolean = false;
+  doneTaskStr: string = "undone";
+  taskTitle:string = "Task";
+  taskDescription:string = "";
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  deleteTask() {
+    this.onDelete.emit(this.index);
+  }
+
+  doneTask() {     
+    this.task.state = TaskState.done;
+    let chs: ChangedState = {index: this.index, state: this.task.state}
+    this.onStateChanged.emit(chs);
+  }
+
+  showChangeForm() {
+    this.taskTitle = this.task.title;
+    this.taskDescription = this.task.description;
+    this.showCahngeForm = !this.showCahngeForm;
+  }
+
+  changeTask() {
+    if (this.taskTitle !== "") {      
+      let st: TaskState;
+      if (this.task.title != this.taskTitle || this.task.description != this.taskDescription) {
+        st = TaskState.undone;
+      }
+      else {        
+        st = this.task.state;
+      }
+      this.task = {title: this.taskTitle, description: this.taskDescription, 
+              state: st, date: new Date() };
+      let cht: ChangedTask = {index: this.index, task: this.task}
+      this.onTaskChanged.emit(cht);
+      this.showCahngeForm = false;
+    }    
+  } 
+  
+  cancelChange(){
+    this.showCahngeForm = false;
+  }
+
+  failTask() {    
+    this.task.state = TaskState.failed;
+    let chs: ChangedState = {index: this.index, state: this.task.state}
+    this.onStateChanged.emit(chs);
+  }
+
+  undoneTask() {
+    this.task.state = TaskState.undone;
+    let chs: ChangedState = {index: this.index, state: this.task.state}
+    this.onStateChanged.emit(chs);
+  }
+}
